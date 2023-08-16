3366620187575e61176cab6656cd58ebe8716c9c
LeulBerhanu <leulberhanua@gmail.com>
2023-08-16 10:44:44 +0300

    setup saga, redux...

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchSongSuccess } from "./Redux/features/songSlice";
 
 function App() {
-  return <div>App</div>;
+  const dispatch = useDispatch();
+  const songs = useSelector((state) => state.songs.value);
+
+  console.log("Songs: ", songs);
+
+  useEffect(() => {
+    dispatch(fetchSongSuccess());
+  }, [dispatch]);
+
+  return (
+    <div>
+      <h1>you better work</h1>
+      {songs.map((song, idx) => (
+        <div key={idx}>{song.name}</div>
+      ))}
+    </div>
+  );
 }
 
 export default App;
