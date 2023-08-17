686ead65e104e65d96963da980d34731d36afd26
LeulBerhanu <leulberhanua@gmail.com>
2023-08-17 18:33:08 +0300

    json server, add song implemented successfully

--- src/api.js
+++ src/api.js
@@ -1,9 +1,10 @@
 import axios from "axios";
 
-const URL = `https://64dc7c1be64a8525a0f68fd5.mockapi.io`;
+// const URL = `https://64dc7c1be64a8525a0f68fd5.mockapi.io`;
+const URL = `http://localhost:3000`;
 
-export const getSongs = () => axios.get(`${URL}/songs`);
-export const createSong = (data) => axios.post(`${URL}/songs`, data);
-export const updateSong = (id, data) =>
+export const getSongsApi = () => axios.get(`${URL}/songs`);
+export const createSongApi = (data) => axios.post(`${URL}/songs`, data);
+export const updateSongApi = (id, data) =>
   axios.put(`${BASE_URL}/users/${id}`, data);
-export const deleteSong = (id) => axios.delete(`${BASE_URL}/users/${id}`);
+export const deleteSongApi = (id) => axios.delete(`${URL}/songs/${id}`);
