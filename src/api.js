c05e3a3044f36522dda6e71113ff837e4641c030
LeulBerhanu <leulberhanua@gmail.com>
2023-08-17 21:37:30 +0300

    axios defaults baseURL

--- src/api.js
+++ src/api.js
@@ -1,10 +1,8 @@
 import axios from "axios";
 
-// const URL = `https://64dc7c1be64a8525a0f68fd5.mockapi.io`;
-const URL = `http://localhost:3000`;
+axios.defaults.baseURL = `http://localhost:3000`;
 
-export const getSongsApi = () => axios.get(`${URL}/songs`);
-export const createSongApi = (data) => axios.post(`${URL}/songs`, data);
-export const updateSongApi = (id, data) =>
-  axios.put(`${URL}/songs/${id}`, data);
-export const deleteSongApi = (id) => axios.delete(`${URL}/songs/${id}`);
+export const getSongsApi = () => axios.get(`/songs`);
+export const createSongApi = (data) => axios.post(`/songs`, data);
+export const updateSongApi = (id, data) => axios.put(`/songs/${id}`, data);
+export const deleteSongApi = (id) => axios.delete(`/songs/${id}`);
