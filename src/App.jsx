c4717e3540e4733b2c0eb456e9ef9aa60233ea74
LeulBerhanu <leulberhanua@gmail.com>
2023-08-18 19:13:20 +0300

    react router implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,20 @@
-import React, { useEffect } from "react";
-import { v4 as uuid } from "uuid";
-import { useDispatch, useSelector } from "react-redux";
-import {
-  getSongFetch,
-  addSong,
-  deleteSongFetch,
-  updateSongFetch,
-} from "./Redux/features/songSlice";
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Layout from "./Components/Layout";
+import Main from "./Components/Main";
+import HomePage from "./Pages/HomePage";
+import LoginPage from "./Pages/LoginPage";
 
 function App() {
-  const dispatch = useDispatch();
-  const songs = useSelector((state) => state.songs.value);
-
-  console.log("Songs: ", songs);
-
-  useEffect(() => {
-    dispatch(getSongFetch());
-  }, [dispatch]);
-
-  function handleAdd() {
-    dispatch(addSong({ id: uuid(), title: "newSong" }));
-  }
-
-  function handleDelete(id) {
-    dispatch(deleteSongFetch({ id }));
-  }
-
-  function handleUpdate(id, data) {
-    dispatch(updateSongFetch({ id, data }));
-  }
-
   return (
-    <div>
-      <h1>music app</h1>
-      {songs.map((song) => (
-        <div key={song.id}>
-          <div>{song.title}</div>
-          <button onClick={() => handleDelete(song.id)}>delete</button>
-          <button onClick={() => handleUpdate(song.id, { title: "leul" })}>
-            update
-          </button>
-        </div>
-      ))}
-
-      <button onClick={() => handleAdd()}>click to add</button>
-    </div>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route element={<Main />}>
+          <Route index element={<HomePage />} />
+          <Route path="login" element={<LoginPage />} />
+        </Route>
+      </Route>
+    </Routes>
   );
 }
 
