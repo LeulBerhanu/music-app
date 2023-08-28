cc2121b379d5748d285ea621b1f07b4523b31b04
LeulBerhanu <leulberhanua@gmail.com>
2023-08-28 18:56:39 +0300

    changed everything

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,68 +1,7 @@
-import { useState } from "react";
-import AddSongForm from "../Components/AddSongForm";
-import styled from "@emotion/styled";
-import { layout, background } from "styled-system";
-import theme from "../theme/theme";
-import { useSelector } from "react-redux";
-
-const AddForm = styled.div`
-  ${layout}
-`;
-
-const StyledHeader = styled.header`
-  ${background}
-  height: 150px;
-  border-radius: 0 0 20px 20px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  flex-direction: column;
-`;
-
-const AddButton = styled.button`
-  ${background}
-  border-radius: 30px;
-  border: none;
-  color: #fff;
-  cursor: pointer;
-  font-size: 2rem;
-  padding: 10px 15px;
-  transition: 0.2s;
-
-  &:hover {
-    background: #584ed8;
-
-    &::after {
-      content: "+";
-      margin-left: 10px;
-    }
-  }
-`;
-
-const Logo = styled.h1`
-  color: #fff;
-  font-size: 5rem;
-`;
+import React from "react";
 
 function Header() {
-  const [addClicked, setAddClicked] = useState(false);
-  const isEditMode = useSelector((state) => state.editMode.value);
-
-  function handleClick() {
-    setAddClicked(true);
-  }
-
-  return (
-    <StyledHeader background="primary_dark">
-      <Logo>MUSICA</Logo>
-      <AddButton background="secondary" onClick={() => handleClick()}>
-        Add a song
-      </AddButton>
-      <AddForm display={addClicked || isEditMode ? "unset" : "none"}>
-        <AddSongForm setAddClicked={setAddClicked} />
-      </AddForm>
-    </StyledHeader>
-  );
+  return <div>Header</div>;
 }
 
 export default Header;
--- src/Components/Songs.jsx
+++ src/Components/Songs.jsx
@@ -1,23 +1,22 @@
-import React from "react";
-import { useDispatch } from "react-redux";
-import { Link } from "react-router-dom";
-import { deleteSongFetch } from "../Redux/features/songSlice";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getSongsFetch } from "../Redux/features/songSlice";
+import Song from "./Song";
 
-function Songs({ song }) {
+function Songs() {
+  const songs = useSelector((state) => state.songs.value);
   const dispatch = useDispatch();
 
-  function handleDelete(id) {
-    dispatch(deleteSongFetch(id));
-  }
+  useEffect(() => {
+    dispatch(getSongsFetch());
+  }, [dispatch]);
 
   return (
-    <li key={song.id}>
-      {song.title}{" "}
-      <Link to={`update-song/${song.id}`}>
-        <button>edit</button>
-      </Link>
-      <button onClick={() => handleDelete(song.id)}>delete</button>
-    </li>
+    <ul>
+      {songs.map((song) => (
+        <Song key={song.id} song={song} />
+      ))}
+    </ul>
   );
 }
 
